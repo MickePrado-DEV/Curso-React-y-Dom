91d49fae823175d77cec8b8b4073b2480de5d80d
Micke Prado <mprado@mosbit.com>
2022-05-26 15:57:00 -0500

    se agregaron iconos

--- src/pages/HomeContainer.jsx
+++ src/pages/HomeContainer.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Header } from './../components/Header';
 export const HomeContainer = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <Header />
     </div>
   )
 }
